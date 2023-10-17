f523796e4f5fa73a9d68159586e221af87cb5927
KOLANICH <kolan_n@mail.ru>
2023-10-17 19:52:53 +0300

    We had to move to Codeberg

--- sunxi_fex/__init__.py
+++ sunxi_fex/__init__.py
@@ -1,1 +1,5 @@
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KFmts/sunxi_fex.py , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
+
 from .kaitai.sunxi_fex import *
